fe3af8c3989d84c56132a422968def376fb7effe
alexvorona <alexvorona@yahoo.com>
2017-11-05 20:31:36 +0200

    new UnitTests added

--- UnitTest/WordCounterUnitTest.cs
+++ UnitTest/WordCounterUnitTest.cs
@@ -30,5 +30,32 @@ namespace UnitTest
             Assert.AreEqual("abc 2", lines[0]);           
         }
 
+        [TestMethod]
+        public void TestDoCountCaseInsensitiveResult()
+        {
+            var counter = new WordCounter.WordCounter();
+            var rows = new string[] { "abc: def, Abc and egh" };
+            counter.CountWords(rows);
+            var lines = counter.GetLines();
+            Assert.AreEqual(4, lines.Length);
+            Assert.AreEqual("abc 2", lines[0]);
+            Assert.AreEqual("and 1", lines[1]);
+            Assert.AreEqual("def 1", lines[2]);
+            Assert.AreEqual("egh 1", lines[3]);
+        }
+
+        [TestMethod]
+        public void TestDoCountManyRowsResult()
+        {
+            var counter = new WordCounter.WordCounter();
+            var rows = new string[] { "abc: def, Abc and egh", "abc: def, Abc and egh" };
+            counter.CountWords(rows);
+            var lines = counter.GetLines();
+            Assert.AreEqual(4, lines.Length);
+            Assert.AreEqual("abc 4", lines[0]);
+            Assert.AreEqual("and 2", lines[1]);
+            Assert.AreEqual("def 2", lines[2]);
+            Assert.AreEqual("egh 2", lines[3]);
+        }
     }
 }
