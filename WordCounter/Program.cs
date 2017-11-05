53edf247bbd67c77a61acc7d8262f50c6859cf9d
alexvorona <alexvorona@yahoo.com>
2017-11-05 14:36:53 +0200

    master

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -7,9 +7,15 @@ using System.Threading.Tasks;
 namespace WordCounter
 {
     class Program
-    {
+    {      
         static void Main(string[] args)
         {
+            var counter = new WordCounter();
+            counter.Read("data1.txt");
+
+            //todo удалить файл если есть!
+            counter.SaveResult("results.txt");           
+            
         }
     }
 }
---
+++ WordCounter/WordCounter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCounter
+{
+    public class WordCounter
+    {
+        static ConcurrentDictionary<string, int> words = new ConcurrentDictionary<string, int>();
+        private static char[] separators = new Char[] { ' ', '.', ',', '-', '«', '»', ':' };
+
+        public void Read(string filename)
+        {
+            Parallel.ForEach(File.ReadLines(filename, Encoding.Default).Where(r => r.ToString() != string.Empty), line =>
+            {
+                DoCount(line);
+            });         
+        }
+        private static void DoCount(string row)
+        {         
+            var res = row.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var word in res)
+            {
+                words.AddOrUpdate(word.ToLower(), 1, (k,v) => v+1);             
+            }
+        }
+        public void SaveResult(string fileName)
+        {
+            var task = Task.Factory.StartNew(() =>
+            {
+                File.WriteAllLines(fileName,
+                 GenerateValues());
+            });
+            task.Wait();
+        }
+
+        private string[] GenerateValues()
+        {
+            var AllLines = new string[words.Count];
+
+            int i = 0;
+            foreach (var item in words.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                AllLines[i]= $"{item.Key} {item.Value}";
+                i++;
+            }
+            return AllLines;
+        }
+    }
+}
